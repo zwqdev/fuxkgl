3781aed350d64ec588c9839a51215862061678f4
cocowa <weiqiangdev@163.com>
2023-09-07 14:44:14 +0800

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -6,15 +6,13 @@ const vertexGlsl = `
   attribute vec2 a_texCoord;
 
   uniform vec2 u_resolution;
-
   varying vec2 v_texCoord;
 
   void main() {
-    vec2 zeroToOne = a_position / u_resolution;
-    vec2 zeroToTwo = zeroToOne * 2.0;
-    vec2 clipSpace = zeroToTwo - 1.0;
-
-    gl_Position = vec4(clipSpace * vec2(1, -1), 0, 1);
+    vec2 zerotoone = a_position / u_resolution;
+    vec2 zerototwo = zerotoone * 2.0;
+    vec2 clipspace = zerototwo - 1.0;
+    gl_Position = vec4(clipspace * vec2(1, -1), 0, 1);
 
     v_texCoord = a_texCoord;
   }
@@ -23,11 +21,24 @@ const fragmentGlsl = `
   precision mediump float;
 
   uniform sampler2D u_image;
-
   varying vec2 v_texCoord;
+  uniform vec2 u_textureSize;
+  uniform float u_kernel[9];
+  uniform float u_kernelWeight;
 
   void main() {
-    gl_FragColor = texture2D(u_image, v_texCoord).bgra;
+    vec2 onePixel = vec2(1.0, 1.0) / u_textureSize;
+    vec4 colorSum =
+     texture2D(u_image, v_texCoord + onePixel * vec2(-1, -1)) * u_kernel[0] +
+     texture2D(u_image, v_texCoord + onePixel * vec2( 0, -1)) * u_kernel[1] +
+     texture2D(u_image, v_texCoord + onePixel * vec2( 1, -1)) * u_kernel[2] +
+     texture2D(u_image, v_texCoord + onePixel * vec2(-1,  0)) * u_kernel[3] +
+     texture2D(u_image, v_texCoord + onePixel * vec2( 0,  0)) * u_kernel[4] +
+     texture2D(u_image, v_texCoord + onePixel * vec2( 1,  0)) * u_kernel[5] +
+     texture2D(u_image, v_texCoord + onePixel * vec2(-1,  1)) * u_kernel[6] +
+     texture2D(u_image, v_texCoord + onePixel * vec2( 0,  1)) * u_kernel[7] +
+     texture2D(u_image, v_texCoord + onePixel * vec2( 1,  1)) * u_kernel[8] ;
+    gl_FragColor = vec4((colorSum / u_kernelWeight).rgb, 1.0);
   }
 `;
 
@@ -51,17 +62,23 @@ const setRectangle = (
 };
 const render = (gl: WebGLRenderingContext, image: HTMLImageElement) => {
   const program = createProgramFromSources(gl, [vertexGlsl, fragmentGlsl]);
+
   if (!program) return;
+  // locations
+  const positionLocation = gl.getAttribLocation(program, "a_position");
+  const texcoordLocation = gl.getAttribLocation(program, "a_texCoord");
+  const resolutionLocation = gl.getUniformLocation(program, "u_resolution");
+  const texturesizeLocation = gl.getUniformLocation(program, "u_textureSize");
+  const kernelLocation = gl.getUniformLocation(program, "u_kernel[0]");
+  const kernelWeightLocation = gl.getUniformLocation(program, "u_kernelWeight");
 
-  const positionLoc = gl.getAttribLocation(program, "a_position");
-  const texcoordLoc = gl.getAttribLocation(program, "a_texCoord");
-
-  const positionBuf = gl.createBuffer();
-  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuf);
+  // set value
+  const positionBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
   setRectangle(gl, 0, 0, image.width, image.height);
 
-  const texcoordBuf = gl.createBuffer();
-  gl.bindBuffer(gl.ARRAY_BUFFER, texcoordBuf);
+  const texcoordBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, texcoordBuffer);
   gl.bufferData(
     gl.ARRAY_BUFFER,
     new Float32Array([
@@ -72,35 +89,40 @@ const render = (gl: WebGLRenderingContext, image: HTMLImageElement) => {
 
   const texture = gl.createTexture();
   gl.bindTexture(gl.TEXTURE_2D, texture);
+  // set paramter
   gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
   gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
   gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
   gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
-
   gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
 
-  const resolutionLoc = gl.getUniformLocation(program, "u_resolution");
-
-  resizeCanvasToDisplaySize(gl.canvas as unknown as HTMLCanvasElement);
+  resizeCanvasToDisplaySize(gl.canvas as HTMLCanvasElement);
 
   gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
 
-  gl.clearColor(0, 0, 0, 0);
   gl.clear(gl.COLOR_BUFFER_BIT);
 
   gl.useProgram(program);
 
-  gl.enableVertexAttribArray(positionLoc);
-  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuf);
+  gl.enableVertexAttribArray(positionLocation);
+  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+  gl.vertexAttribPointer(positionLocation, 2, gl.FLOAT, false, 0, 0);
 
-  gl.vertexAttribPointer(positionLoc, 2, gl.FLOAT, false, 0, 0);
+  gl.enableVertexAttribArray(texcoordLocation);
+  gl.bindBuffer(gl.ARRAY_BUFFER, texcoordBuffer);
+  gl.vertexAttribPointer(texcoordLocation, 2, gl.FLOAT, false, 0, 0);
 
-  gl.enableVertexAttribArray(texcoordLoc);
-  gl.bindBuffer(gl.ARRAY_BUFFER, texcoordBuf);
+  gl.uniform2f(resolutionLocation, gl.canvas.width, gl.canvas.height);
+  gl.uniform2f(texturesizeLocation, image.width, image.height);
 
-  gl.vertexAttribPointer(texcoordLoc, 2, gl.FLOAT, false, 0, 0);
+  const edgeDetectKernel = [-1, -1, -1, -1, 8, -1, -1, -1, -1];
 
-  gl.uniform2f(resolutionLoc, gl.canvas.width, gl.canvas.height);
+  const weight =
+    edgeDetectKernel.reduce((prev, current) => prev + current, 0) || 1;
+
+  gl.uniform1fv(kernelLocation, edgeDetectKernel);
+
+  gl.uniform1f(kernelWeightLocation, weight);
 
   gl.drawArrays(gl.TRIANGLES, 0, 6);
 };
@@ -115,6 +137,6 @@ const App: FC = () => {
       render(gl, image);
     };
   });
-  return <canvas ref={ref}></canvas>;
+  return <canvas ref={ref} width={400} height={300}></canvas>;
 };
 export default App;
