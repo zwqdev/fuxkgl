a783d8aa11a85910f6a2537723757736a5b32608
cocowa <weiqiangdev@163.com>
2023-09-07 15:34:37 +0800

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -60,6 +60,17 @@ const setRectangle = (
     gl.STATIC_DRAW
   );
 };
+
+function createAndSetupTexture(gl: WebGL2RenderingContext) {
+  const texture = gl.createTexture();
+  gl.bindTexture(gl.TEXTURE_2D, texture);
+  // 设置材质，这样我们可以对任意大小的图像进行像素操作
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+  return texture;
+}
 const render = (gl: WebGLRenderingContext, image: HTMLImageElement) => {
   const program = createProgramFromSources(gl, [vertexGlsl, fragmentGlsl]);
 
